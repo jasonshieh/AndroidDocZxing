8cb1c0da67cb02b11c34a9e663aea7288ee0058f
xiexiaojian <xie.xiaojian@whaley.cn>
2016-12-28 20:08:01 +0800

    Initial commit

---
+++ app/src/main/java/zxing/androiddoc/com/androiddoczxing/MainActivity.java
@@ -0,0 +1,13 @@
+package zxing.androiddoc.com.androiddoczxing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
